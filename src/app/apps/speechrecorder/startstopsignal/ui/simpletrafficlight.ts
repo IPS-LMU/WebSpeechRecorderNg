47163edaa3d0ae90728e92e4dd42bf7d8236f49d
klausj <klausj@phonetik.uni-muenchen.de>
2017-06-13 22:47:23 +0200

    Build Ng simple traffic light component.

--- src/app/apps/speechrecorder/startstopsignal/ui/simpletrafficlight.ts
+++ src/app/apps/speechrecorder/startstopsignal/ui/simpletrafficlight.ts
@@ -1,6 +1,38 @@
+import { Component } from '@angular/core'
 import { StartStopSignal,State } from '../startstopsignal'
 
+@Component({
 
+    selector: 'app-simpletrafficlight',
+
+    template: `
+
+        <div>
+            <div id="stl_red" class="circle"></div>
+            <div id="stl_yellow" class="circle"></div>
+            <div id="stl_green" class="circle"></div>
+        </div>
+    `
+    ,
+    styles: [`.circle {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin: 5px;
+        background: grey;
+    //flex: 1 1 100%;
+    }`,
+    `:host{
+                        display: flex;
+                        flex-direction: column;
+                        background: black;
+                        padding: 2px;
+                        height: 170px;
+                        max-height: 170px;
+                        flex: 0;
+      }
+    `]
+})
     export class SimpleTrafficLight implements StartStopSignal {
         red:HTMLElement;
         yellow:HTMLElement;
@@ -10,7 +42,7 @@ import { StartStopSignal,State } from '../startstopsignal'
             this.red = document.getElementById('stl_red');
             this.yellow = document.getElementById('stl_yellow');
             this.green = document.getElementById('stl_green');
-            this.setStatus(State.OFF);
+            //this.setStatus(State.OFF);
         }
 
         setStatus(status:State) {
