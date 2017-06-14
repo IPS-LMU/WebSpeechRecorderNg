df017886ba3a6139c2e2f3c8ab7db07637171e44
klausj <klausj@phonetik.uni-muenchen.de>
2017-06-14 12:14:28 +0200

    Traffic light using Angular!

--- src/app/apps/speechrecorder/startstopsignal/ui/simpletrafficlight.ts
+++ src/app/apps/speechrecorder/startstopsignal/ui/simpletrafficlight.ts
@@ -1,73 +1,84 @@
-import { Component } from '@angular/core'
-import { StartStopSignal,State } from '../startstopsignal'
+import {Component} from '@angular/core'
+import {StartStopSignal, State} from '../startstopsignal'
 
 @Component({
 
-    selector: 'app-simpletrafficlight',
+  selector: 'app-simpletrafficlight',
 
-    template: `
+  template: `
 
-        <div>
-            <div id="stl_red" class="circle"></div>
-            <div id="stl_yellow" class="circle"></div>
-            <div id="stl_green" class="circle"></div>
-        </div>
-    `
-    ,
-    styles: [`.circle {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        margin: 5px;
-        background: grey;
-    //flex: 1 1 100%;
+    <div>
+      <div class="circle {{lighttop}}"></div>
+      <div class="circle {{lightmid}}"></div>
+      <div class="circle {{lightbottom}}"></div>
+    </div>
+  `
+  ,
+  styles: [`.circle {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin: 5px;
+    background: grey;
+  }`, `.red {
+    background: red;
+  }
+  `, `
+    .yellow {
+      background: yellow;
+    }
+  `, `
+    .green {
+      background: green;
+    }
+  `, `
+    .black {
+      background: black;
     }`,
-    `:host{
-                        display: flex;
-                        flex-direction: column;
-                        background: black;
-                        padding: 2px;
-                        height: 170px;
-                        max-height: 170px;
-                        flex: 0;
-      }
+      `:host {
+      display: flex;
+      flex-direction: column;
+      background: black;
+      padding: 2px;
+      height: 170px;
+      max-height: 170px;
+      flex: 0;
+    }
     `]
 })
-    export class SimpleTrafficLight implements StartStopSignal {
-        red:HTMLElement;
-        yellow:HTMLElement;
-        green:HTMLElement;
+export class SimpleTrafficLight implements StartStopSignal {
 
-        constructor() {
-            this.red = document.getElementById('stl_red');
-            this.yellow = document.getElementById('stl_yellow');
-            this.green = document.getElementById('stl_green');
-            //this.setStatus(State.OFF);
-        }
+  private lighttop: string;
+  private lightmid: string;
+  private lightbottom: string;
 
-        setStatus(status:State) {
-            if (State.OFF === status) {
-                this.red.style.background = 'grey';
-                this.yellow.style.background = 'grey';
-                this.green.style.background = 'grey';
-            } else if (State.IDLE === status) {
-                this.red.style.background = 'red';
-                this.yellow.style.background = 'grey';
-                this.green.style.background = 'grey';
-            } else if (State.PRERECORDING == status) {
-                this.red.style.background = 'red';
-                this.yellow.style.background = 'yellow';
-                this.green.style.background = 'grey';
-            } else if (State.RECORDING == status) {
-                this.red.style.background = 'grey';
-                this.yellow.style.background = 'grey';
-                this.green.style.background = 'green';
-            } else if (State.POSTRECORDING == status) {
-                this.red.style.background = 'grey';
-                this.yellow.style.background = 'yellow';
-                this.green.style.background = 'grey';
-            }
-        }
+  constructor() {
+    this.setStatus(State.OFF);
+  }
+
+  setStatus(status: State) {
+    if (State.OFF === status) {
+      this.lighttop = 'grey';
+      this.lightmid = 'grey';
+      this.lightbottom = 'grey';
+    } else if (State.IDLE === status) {
+      this.lighttop = 'red';
+      this.lightmid = 'grey';
+      this.lightbottom = 'grey';
+    } else if (State.PRERECORDING == status) {
+      this.lighttop = 'red';
+      this.lightmid = 'yellow';
+      this.lightbottom = 'grey';
+    } else if (State.RECORDING == status) {
+      this.lighttop = 'grey';
+      this.lightmid = 'grey';
+      this.lightbottom = 'green';
+    } else if (State.POSTRECORDING == status) {
+      this.lighttop = 'grey';
+      this.lightmid = 'yellow';
+      this.lightbottom = 'grey';
     }
+  }
+}
 
 
