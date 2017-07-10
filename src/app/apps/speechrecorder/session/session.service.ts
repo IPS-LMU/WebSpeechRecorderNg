08b10ade0a17c30fb0859c340351529048b53873
klausj <klausj@phonetik.uni-muenchen.de>
2017-07-10 16:59:42 +0200

    Use (REST) API endpoint and type from environment.

--- src/app/apps/speechrecorder/session/session.service.ts
+++ src/app/apps/speechrecorder/session/session.service.ts
@@ -1,30 +1,40 @@
-/**
- * Created by klausj on 17.06.2017.
- */
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
+import {environment} from "../../../../environments/environment";
 
 
 @Injectable()
 export class SessionService {
-    private sessionsUrl = 'test/session/';  // URL to web api
-    constructor(private http: Http) {
+  private sessionsApiCtx = 'session';  // URL to web api
+  private sessionsUrl;
 
+  constructor(private http: Http) {
+
+    let apiEndPoint = environment.apiEndPoint ? environment.apiEndPoint : 'api';
+
+    this.sessionsUrl = apiEndPoint + '/' + this.sessionsApiCtx;
+  }
+
+  getSession(id: string): Promise<any> {
+
+    let sessUrl = this.sessionsUrl + '/' + id;
+    if (environment.apiType === 'files') {
+      // for development and demo
+      sessUrl = sessUrl + '.json';
     }
-   getSession(id:string):Promise<any>{
+    let sessProms = this.http.get(sessUrl).toPromise()
+      .then(response => {
+        return response.json();
+      })
+      .catch(this.handleError);
+    return sessProms;
+  }
 
-        // TODO REST without .json extension !!
-       let sessProms=this.http.get(this.sessionsUrl+id+'.json').toPromise()
-           .then(response => {return response.json();})
-           .catch(this.handleError);
-       return sessProms;
-   }
-
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
 
 
