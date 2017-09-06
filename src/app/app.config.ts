fc483d88cc2163166898375aeec9699d29bdc1b0
klausj <klausj@phonetik.uni-muenchen.de>
2017-09-06 17:41:29 +0200

    Enabled doenload of recordings for demo/development mode.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,8 @@ import {ApiType, SpeechRecorderConfig} from "../module/speechrecorder/spr.config
 export const SPR_CFG: SpeechRecorderConfig = {
   apiEndPoint: 'test',
   apiType: ApiType.FILES,
-  enableDownloadRecordings: true
-
+  enableDownloadRecordings: true,
+  enableUploadRecordings: false
 };
 
 
