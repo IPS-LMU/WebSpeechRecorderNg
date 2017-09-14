0971106c45c0be88eca6361e93769b8900157505
klausj <klausj@phonetik.uni-muenchen.de>
2017-09-14 16:40:04 +0200

    Added stream chunker to update level display at specific periods.

--- src/module/io/stream.ts
+++ src/module/io/stream.ts
@@ -1,7 +1,91 @@
 
-export interface Float32OutStream{
+export interface Float32ArrayOutStream{
 
     write(buffers:Array<Float32Array>):number;
     flush():void;
     close():void;
+}
+
+
+
+export class Float32ArrayChunkerOutStream implements Float32ArrayOutStream{
+
+  private bufs=new Array<Float32Array>();
+  private filled:number;
+
+  private _channels:number;
+  private _chunkSize:number;
+
+  constructor(private outStream:Float32ArrayOutStream){
+
+    this.filled=0;
+
+  }
+
+  private createBuffers(){
+    this.bufs=new Array<Float32Array>(this._channels);
+    for(let ch=0;ch<this._channels;ch++){
+      this.bufs[ch]=new Float32Array(this._chunkSize);
+    }
+  }
+
+  set chunkSize(chunkSize:number){
+    this._chunkSize=chunkSize;
+    this.createBuffers();
+  }
+
+  set channels(channels:number){
+    this._channels=channels;
+    this.createBuffers();
+  }
+
+
+  write(buffers:Array<Float32Array>):number{
+
+      let copied=0;
+      let buffersLen=buffers[0].length;
+      let avail=buffersLen;
+      // Fill out buffers until all values copied
+      while (avail>0) {
+        let toFill=this._chunkSize-this.filled;
+        if(toFill>avail){
+          toFill=avail;
+        }
+        let sliceEnd=copied+toFill;
+
+        for (let ch = 0; ch < this._channels; ch++) {
+          let cpPrt = buffers[ch].slice(copied,sliceEnd);
+          let prtLen=cpPrt.length;
+          let buf=this.bufs[ch];
+          let bufLen=buf.length;
+          if(bufLen<this.filled+prtLen){
+            console.error(bufLen+" < "+this.filled+" + "+prtLen);
+          }else {
+            buf.set(cpPrt, this.filled);
+          }
+        }
+        copied+=toFill;
+        avail-=copied;
+        this.filled+=toFill;
+        if(this.filled==this._chunkSize){
+          this.outStream.write(this.bufs);
+          this.filled=0;
+
+        }
+      }
+    return copied;
+  }
+  flush():void{
+    if(this.filled>0){
+      let restBufs=new Array<Float32Array>(this._channels);
+      for (let ch = 0; ch < this._channels; ch++) {
+        restBufs[ch] = this.bufs[ch].slice(0,this.filled);
+      }
+      this.outStream.write(restBufs);
+      this.outStream.flush();
+    }
+  }
+  close():void{
+    this.outStream.close();
+  }
 }
