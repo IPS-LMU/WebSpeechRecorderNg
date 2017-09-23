e286fd8d1eac2decf331486dd03d4009ec6fa511
klausj <klausj@phonetik.uni-muenchen.de>
2017-09-23 21:10:38 +0200

    Aded Intl polyfill for Firefox for Android.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -66,8 +66,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+// Required for Android Firefox
+import 'intl';  // Run `npm install --save intl`.
 /**
  * Need to import at least one locale-data with intl.
  */
-// import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/de';
