dff4d50584176acbc5b28431c1f5a1bb42216735
klausj <klausj@phonetik.uni-muenchen.de>
2018-12-01 19:29:03 +0100

    Only one audio player.

--- projects/speechrecorderng/src/lib/audio/audio_player.ts
+++ projects/speechrecorderng/src/lib/audio/audio_player.ts
@@ -42,7 +42,7 @@ import {AudioDisplayScrollPane} from "./ui/audio_display_scroll_pane";
     }`]
 
 })
-export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked {
+export class AudioDisplay implements AudioPlayerListener, OnInit,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked {
   private _audioUrl: string;
 
   parentE: HTMLElement;
@@ -57,8 +57,8 @@ export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked
   zoomOutAction:Action;
 
 
- // aCtx: AudioContext;
-
+  aCtx: AudioContext;
+  ap: AudioPlayer;
   status: string;
 
   currentLoader: XMLHttpRequest | null;
@@ -86,7 +86,13 @@ export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked
     this.zoomInAction=this.ac.zoomInAction;
       var n = <any>navigator;
       var w = <any>window;
-
+      AudioContext = w.AudioContext || w.webkitAudioContext;
+      if (typeof AudioContext !== 'function') {
+          this.status = 'ERROR: Browser does not support Web Audio API!';
+      } else {
+          this.aCtx = new AudioContext();
+          this.ap = new AudioPlayer(this.aCtx, this);
+      }
   }
 
   ngAfterContentInit(){
@@ -98,7 +104,10 @@ export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked
   }
 
   ngAfterViewInit() {
-
+      if (this.aCtx && this.ap) {
+          this.playStartAction.onAction = () => this.ap.start();
+          this.playStopAction.onAction = () => this.ap.stop();
+      }
       this.layout();
       let heightListener=new MutationObserver((mrs:Array<MutationRecord>,mo:MutationObserver)=>{
           mrs.forEach((mr:MutationRecord)=>{
@@ -108,7 +117,11 @@ export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked
           })
       });
       heightListener.observe(this.parentE,{attributes: true,childList: true, characterData: true});
-
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['url']) {
+        this.audioUrl = params['url'];
+      }
+    });
   }
 
 
@@ -126,27 +139,104 @@ export class AudioDisplay implements OnInit,AfterContentInit,AfterContentChecked
     this.ac.layout();
   }
 
+  get audioUrl(): string {
+    return this._audioUrl;
+  }
+
+  set audioUrl(value: string) {
+    this.ap.stop();
+    this._audioUrl = value;
+    this.load();
+  }
+
 
   started() {
     console.log("Play started");
     this.status = 'Playing...';
   }
 
+  private load() {
+
+    if (this.currentLoader) {
+      this.currentLoader.abort();
+      this.currentLoader = null;
+    }
+    //this.statusMsg.innerHTML = 'Connecting...';
+    this.currentLoader = new XMLHttpRequest();
+    this.currentLoader.open("GET", this._audioUrl, true);
+    this.currentLoader.responseType = "arraybuffer";
+    this.currentLoader.onload = (e) => {
+      if (this.currentLoader) {
+
+        var data = this.currentLoader.response; // not responseText
+        console.log("Received data ", data.byteLength);
+        this.currentLoader = null;
+        this.loaded(data);
+      }
+    }
+    this.currentLoader.onerror = (e) => {
+      console.log("Error downloading ...");
+      //this.statusMsg.innerHTML = 'Error loading audio file!';
+      this.currentLoader = null;
+    }
+    //this.statusMsg.innerHTML = 'Loading...';
+
+    this.currentLoader.send();
+
+  }
+
+  private loaded(data: ArrayBuffer) {
+
+    console.log("Loaded");
+    this.status = 'Audio file loaded.';
+    console.log("Received data ", data.byteLength);
+
+    var audioBuffer = this.aCtx.decodeAudioData(data, (audioBuffer) => {
+      console.log("Samplerate: ", audioBuffer.sampleRate);
+      this.audioData=audioBuffer;
+    });
+  }
 
   @Input()
   set audioData(audioBuffer: AudioBuffer){
       this.ac.audioData = audioBuffer;
       if(audioBuffer) {
           let clip = new AudioClip(audioBuffer);
-
+          if (this.ap){
+              this.ap.audioClip = clip;
+                this.playStartAction.disabled = false
+            }
       }else{
           this.playStartAction.disabled = true
+          if (this.ap){
+              this.ap.audioClip = null;
+          }
       }
   }
 
 
-  set playFramePosition(playFramePosition:number){
-      this.ac.playFramePosition = playFramePosition
+  updatePlayPosition() {
+
+    if (this.ap && this.ap.playPositionFrames) {
+      this.ac.playFramePosition = this.ap.playPositionFrames;
+    }
+  }
+
+  audioPlayerUpdate(e: AudioPlayerEvent) {
+    if (EventType.STARTED === e.type) {
+      this.status = 'Playback...';
+      this.updateTimerId = window.setInterval(e => this.updatePlayPosition(), 50);
+      this.playStartAction.disabled = true;
+      this.playStopAction.disabled = false;
+    } else if (EventType.ENDED === e.type) {
+      this.status = 'Ready.';
+      window.clearInterval(this.updateTimerId);
+      this.playStartAction.disabled = false;
+      this.playStopAction.disabled = true;
+    }
+
+    this.ref.detectChanges();
+
   }
 
   error() {
