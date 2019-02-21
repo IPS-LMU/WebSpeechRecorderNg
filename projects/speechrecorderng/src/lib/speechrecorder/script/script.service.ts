4e6ab0b1d44242ff9588fd1763acc9f58108fb5d
klausj <klausj@phonetik.uni-muenchen.de>
2019-02-21 01:49:19 +0100

    Use generic super method to add script.

--- projects/speechrecorderng/src/lib/speechrecorder/script/script.service.ts
+++ projects/speechrecorderng/src/lib/speechrecorder/script/script.service.ts
@@ -86,90 +86,31 @@ export class ScriptService extends GenericSprService<Script>{
       scrsUrl = scrsUrl + '/_list.json?requestUUID='+UUID.generate();
 
     }else{
+        // Some projects may have many scripts, limit the count of fetched scripts and let them be ordered by the server:
+        // Scripts with fewer count of sessions first (lowest usage).
+        // This should make sure that the scripts are used evenly
       httpParams.append('order-direction','DESC')
       httpParams.append('order-by', 'sessions._size')
       httpParams.append('limit',ScriptService.SCRIPTS_FETCH_COUNT_LIMIT.toString())
     }
 
-    let obs=new Observable<Array<Script>>(subscriber => {
-        let scripts;
-      let obsHttp = this.http.get<Array<Script>>(scrsUrl, {params:httpParams,withCredentials: this.withCredentials});
-      obsHttp.subscribe(nextScripts => {
-          scripts=nextScripts;
-      }, err => {
-        console.info("Fetching scripts from server failed")
-        let obs = this.sprDb.prepare();
-        obs.subscribe(value => {
-              let scrsTr = value.transaction(ScriptService.SCRIPT_KEYNAME)
-              let sSto = scrsTr.objectStore(ScriptService.SCRIPT_KEYNAME);
-              // TODO only scripts for certain project
-              let allS = sSto.getAll();
-              allS.onsuccess=(ev)=>{
-                console.info("Found " + allS.result.length + " scripts in indexed db")
-                subscriber.next(<Array<Script>>allS.result);
+    return this.getAndCacheEntities(scrsUrl,httpParams,'projectIdx',[projectName])
 
-              }
-              scrsTr.oncomplete=(ev)=>{
-                subscriber.complete()
-              }
+  }
 
-            }, (err) => {
-              console.info("Db store not available")
-              subscriber.error(err)
+  addProjectScript(projectName:string,script:Script):Observable<Script>{
+      let httpParams=new HttpParams()
 
-            }, () => {
-              //subscriber.complete()
-            }
-        )
-      }, () => {
-          // OK fresh data from server
-          // save to indexed db
-          let scrsObs = this.sprDb.prepare();
-          scrsObs.subscribe(db => {
-              let scrTr = db.transaction('script','readwrite')
-              let scrSto = scrTr.objectStore('script');
-              // delete old scripts
-              let scrPrjIdx=scrSto.index('projectIdx')
-              let r=scrPrjIdx.getAllKeys(IDBKeyRange.only([projectName]));
-              r.onsuccess= (ev) => {
-                  r.result.forEach((k)=>{
-                      scrSto.delete(k)
-                  })
-                  scripts.forEach((scr)=>{
-                      scr.project=projectName
-                      scrSto.put(scr)
-                  })
-              }
+      let scrUrl = this.apiEndPoint+ '/'+ProjectService.PROJECT_API_CTX +'/'+projectName+'/'+ScriptService.SCRIPT_KEYNAME+'/'+script.scriptId
 
 
-              scrTr.oncomplete = () => {
-                  console.log("Scripts store transaction completed")
-                  subscriber.next(scripts)
-                  subscriber.complete()
-              }
-              scrTr.onerror = () => {
-                  // We have frech scripts from server
-                  // Proceed though indexed db failed
-                  console.error("Failed to store scripts from server")
-                  subscriber.next(scripts)
-                  subscriber.complete()
+      if (this.config && this.config.apiType === ApiType.FILES) {
+          // for development and demo
+          // append UUID to make request URL unique to avoid localhost server caching
+          scrUrl = scrUrl + '/.json?requestUUID='+UUID.generate();
 
-              }
-          },(err)=>{
-              // We have fresh scripts from server
-              // Proceed though indexed db failed
-              console.error("Failed to store scripts from server")
-              subscriber.next(scripts)
-              subscriber.complete()
-
-          })
-
-
-      });
-    })
-    return obs
-
-
+      }
+      return this.addEntityObserver(script,script.scriptId,scrUrl);
   }
 
 
