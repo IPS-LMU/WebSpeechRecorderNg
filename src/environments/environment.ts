8d31c83a40196462ea8cf593ef05522503ea5a23
klausj <klausj@phonetik.uni-muenchen.de>
2019-03-13 19:20:50 +0100

    Updated all dep packages. Version 0.54.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,12 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 import {ApiType, SpeechRecorderConfig} from "../../projects/speechrecorderng/src/lib/spr.config";
 
 export const environment = {
-  production: false,
-  apiType: 'standalone',
-  apiEndPoint: 'test'
+  production: true,
+  apiEndPoint: 'api/v1'
 };
-
 export const SPR_CFG: SpeechRecorderConfig = {
-  apiEndPoint: 'test',
-  apiType: ApiType.STANDALONE,
+  apiEndPoint: 'api/v1',
+  apiType: ApiType.NORMAL,
   enableDownloadRecordings: true,
-  enableUploadRecordings: false
+  enableUploadRecordings: true
 };
-
