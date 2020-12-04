edf19489c063901dbf3d92e2cc1ec2e23304c0d2
klausj <klausj@phonetik.uni-muenchen.de>
2020-12-04 15:26:23 +0100

    Update @angular/cli

--- projects/speechrecorderng/src/lib/speechrecorderng.component.spec.ts
+++ projects/speechrecorderng/src/lib/speechrecorderng.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SpeechrecorderngComponent} from './speechrecorderng.component';
 
@@ -6,7 +6,7 @@ describe('SpeechrecorderngComponent', () => {
   let component: SpeechrecorderngComponent;
   let fixture: ComponentFixture<SpeechrecorderngComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SpeechrecorderngComponent ]
     })
