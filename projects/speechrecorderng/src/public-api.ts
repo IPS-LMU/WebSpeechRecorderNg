c90b8aaea39989569f062d2a8577f4d6f2bae226
klausj <klausj@phonetik.uni-muenchen.de>
2021-05-19 17:36:26 +0200

    Added fresh new configuration files to compile with Angular 12.

---
+++ projects/speechrecorderng/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of speechrecorderng
+ */
+
+export * from './lib/speechrecorderng.service';
+export * from './lib/speechrecorderng.component';
+export * from './lib/speechrecorderng.module';
