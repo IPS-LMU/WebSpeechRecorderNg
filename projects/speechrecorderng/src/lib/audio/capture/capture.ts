f683e3d8c0b1f166f5d3c9e6c8974a8a26aa6664
klausj <klausj@phonetik.uni-muenchen.de>
2021-06-22 20:11:44 +0200

    Code cleanup.

--- projects/speechrecorderng/src/lib/audio/capture/capture.ts
+++ projects/speechrecorderng/src/lib/audio/capture/capture.ts
@@ -88,56 +88,7 @@ export interface AudioCaptureListener {
 }
 
 
-
-// class AudioCaptureProcessor extends AudioWorkletProcessor{
-//   set data(value: Float32Array[][] | null) {
-//     this._data = value;
-//   }
-//   set audioOutStream(value: SequenceAudioFloat32OutStream | null) {
-//     this._audioOutStream = value;
-//   }
-//   set capturing(value: boolean) {
-//     this._capturing = value;
-//   }
-//
-//   private _capturing:boolean=false;
-//   private _data:Float32Array[][]|null=null;
-//   private _audioOutStream:SequenceAudioFloat32OutStream|null=null;
-//   private _framesRecorded=0;
-//  process(
-//       inputs: Float32Array[][],
-//       outputs: Float32Array[][],
-//       parameters: Record<string, Float32Array>
-//   ): boolean {
-//     if (this.capturing && this._data) {
-//       let inputsCnt=inputs.length;
-//       for(let ii=0;ii<inputsCnt;ii++) {
-//         let channelCount = inputs[ii].length;
-//         let currentBuffers = new Array<Float32Array>(channelCount);
-//         for (let ch: number = 0; ch < channelCount; ch++) {
-//           let chSamples = inputs[ii][ch];
-//           let chSamplesCopy = chSamples.slice(0);
-//           currentBuffers[ch] = chSamplesCopy.slice(0);
-//           this._data[ch].push(chSamplesCopy);
-//           this._framesRecorded += chSamplesCopy.length;
-//         }
-//         //c++;
-//         if (this.audioOutStream) {
-//           this.audioOutStream.write(currentBuffers);
-//         }
-//       }
-//     }
-//     return true;
-//
-//   }
-// }
-
-
-
 export class AudioCapture{
-
-  //readonly port: MessagePort=new MessagePort();
-
 
   get opened(): boolean {
     return this._opened;
@@ -146,10 +97,6 @@ export class AudioCapture{
   static BUFFER_SIZE: number = 8192;
   context: any;
   stream!: MediaStream;
-  //mediaStream:MediaStreamAudioSourceNode;
-  // no d.ts for Web audio API found so far (tsd query *audio*) (Nov 2015)
-  // TODO use AudioRecorder
-
   channelCount!: number;
   mediaStream: any;
   bufferingNode: AudioNode|null=null;
