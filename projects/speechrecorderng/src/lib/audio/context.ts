ffe61c53b466c3fbc85cea70389c0c618b729d02
klausj <klausj@phonetik.uni-muenchen.de>
2021-10-22 20:05:59 +0200

    Accept webkit prefixed audio context.

--- projects/speechrecorderng/src/lib/audio/context.ts
+++ projects/speechrecorderng/src/lib/audio/context.ts
@@ -8,8 +8,15 @@ export class AudioContextProvider
     if (!this._audioContext) {
       let debugFail = false;
       if (!window.AudioContext || typeof window.AudioContext !== 'function' || debugFail) {
-        throw new Error('Browser does not support Web Audio API!');
-        this._audioContext=null;
+        // Typecast to any to query prefixed APIs
+        let w:any=window;
+        // Check Safari webkit
+        if(!w.webkitAudioContext) {
+          throw new Error('Browser does not support Web Audio API!');
+          this._audioContext = null;
+        }else{
+          this._audioContext= <AudioContext> new w.webkitAudioContext();
+        }
       } else {
         this._audioContext= new window.AudioContext();
       }
