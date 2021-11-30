f6684d8403fcfde316a1d7eaf1bd9c3976385074
klausj <klausj@phonetik.uni-muenchen.de>
2021-11-30 20:11:20 +0100

    Fixed: Session finished not detected if nonrecording prompts are used. GitHub issue #28.

--- projects/speechrecorderng/src/lib/speechrecorder/session/item.ts
+++ projects/speechrecorderng/src/lib/speechrecorder/session/item.ts
@@ -1,14 +1,24 @@
 import {RecordingFile} from "../recording";
 
 export class Item {
-    promptAsString: string;
-    training: boolean;
     recs: Array<RecordingFile> | null;
 
     constructor(promptAsString: string, training: boolean) {
         this.promptAsString = promptAsString;
         this.training = training;
         this.recs = null;
+    }
+
+    get promptAsString():string{
+        return this._promptAsString;
+    }
+
+    get training():boolean{
+        return this._training;
+    }
+
+    get recording():boolean{
+        return this._recording;
     }
 
 }
