e6cb3129efbc2f75447ef300418662a1f5f201a6
klausj <klausj@phonetik.uni-muenchen.de>
2022-04-22 10:21:16 +0200

    Removed duplicate export.

--- projects/speechrecorderng/src/lib/spr.module.version.ts
+++ projects/speechrecorderng/src/lib/spr.module.version.ts
@@ -1,1 +1,1 @@
-export const VERSION='2.100.0-alpha.109'
+export const VERSION='2.100.0-alpha.110'
--- projects/speechrecorderng/src/public-api.ts
+++ projects/speechrecorderng/src/public-api.ts
@@ -14,7 +14,6 @@ export {Action} from "./lib/action/action";
 export {ClassDescriptor,PropertyDescriptor,DataType,UiFieldHint,LocalizedMessage} from './lib/reflect/class_descriptor'
 
 export {FitToPageComponent,FitToPageUtil} from "./lib/ui/fit_to_page_comp"
-export {ReadyStateProvider} from "./lib/recorder_component"
 export {MessageDialog} from "./lib/ui/message_dialog"
 export {ReadyStateProvider} from "./lib/recorder_component"
 export {AudioClip,Selection} from './lib/audio/persistor'
