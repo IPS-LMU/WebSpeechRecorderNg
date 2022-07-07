c142aa4ec1cc5bb28d2dd4f9242c9eb08794f9e5
klausj <klausj@phonetik.uni-muenchen.de>
2022-07-07 21:14:57 +0200

    Fixed speechrceorder comp test.

--- projects/speechrecorderng/src/lib/speechrecorderng.component.spec.ts
+++ projects/speechrecorderng/src/lib/speechrecorderng.component.spec.ts
@@ -4,26 +4,28 @@ import { SpeechrecorderngComponent} from './speechrecorderng.component';
 import {RouterTestingModule} from "@angular/router/testing";
 import {SpeechrecorderngService} from "./speechrecorderng.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {SpeechrecorderngModule} from "./speechrecorderng.module";
+import {SPR_CFG} from "../../../../src/app/app.config";
 
 describe('SpeechrecorderngComponent', () => {
   let component: SpeechrecorderngComponent;
   let fixture: ComponentFixture<SpeechrecorderngComponent>;
 
-  // beforeEach(waitForAsync(() => {
-  //   TestBed.configureTestingModule({
-  //     imports: [HttpClientTestingModule,RouterTestingModule],
-  //     declarations: [ SpeechrecorderngComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,RouterTestingModule,SpeechrecorderngModule.forRoot(SPR_CFG)],
+      declarations: [ SpeechrecorderngComponent ]
+    })
+    .compileComponents();
+  }));
   //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(SpeechrecorderngComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpeechrecorderngComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
   //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
