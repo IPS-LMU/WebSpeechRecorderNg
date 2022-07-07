dec73f8f243c9df9aacdb71b922f8b56c0361e15
klausj <klausj@phonetik.uni-muenchen.de>
2022-07-07 17:47:08 +0200

    Added method to retrieve particular frames in a buffer.

--- projects/speechrecorderng/src/lib/audio/array_audio_buffer.ts
+++ projects/speechrecorderng/src/lib/audio/array_audio_buffer.ts
@@ -20,6 +20,55 @@ export class ArrayAudioBuffer {
     return this._channelCount;
   }
 
+
+
+  frames(framePos:number,frameLen:number,bufs:Float32Array[]):number{
+
+    let ccFramePos=0;
+    let trgFramePos=framePos;
+    let ch0Data = this.data[0];
+    let cPos=0;
+    let filled=0;
+    let ci=0;
+
+    while(filled<frameLen && ci<this._chunkCount){
+      // Current chunk
+      let cc0=ch0Data[ci];
+      let ccLen=cc0.length;
+      let ccFrameEndPos=ccFramePos+ccLen;
+
+      if(trgFramePos>=ccFramePos && trgFramePos<ccFrameEndPos){
+            let toCp=frameLen-filled;
+            cPos=framePos-ccFramePos;
+        if(cPos+toCp>ccLen){
+          toCp=ccLen-cPos;
+        }
+            for(let ch=0;ch<bufs.length;ch++){
+              let cc=this.data[ch][ci];
+
+              for(let i=0;i<toCp;i++){
+                bufs[ch][filled+i]=cc[cPos+i];
+              }
+            }
+            filled+=toCp;
+            cPos+=toCp;
+            if(cPos>=ccLen){
+              ccFramePos=ccFrameEndPos;
+              cPos=0;
+              ci++;
+            }
+
+      }else{
+        // next chunk
+        ccFramePos=ccFrameEndPos;
+        cPos=0;
+        ci++;
+      }
+    }
+    return filled;
+  }
+
+
   get sampleRate(): number {
     return this._sampleRate;
   }
@@ -39,8 +88,9 @@ export class ArrayAudioBuffer {
 }
 
 
-export class ArrayAudioBufferInputsStream implements Float32ArrayInputStream{
-  private framePos=0;
+export class ArrayAudioBufferInputStream implements Float32ArrayInputStream{
+
+  private _framePos=0;
   private chunkFramePos=0;
   private chunkIdx=0;
   private eod=false;
@@ -84,12 +134,16 @@ export class ArrayAudioBufferInputsStream implements Float32ArrayInputStream{
           }
         }
       }
-    this.framePos+=read;
+    this._framePos+=read;
       //console.debug("Read: "+read+", frame pos: "+this.framePos)
-    if(this.framePos>this.arrayAudioBuffer.frameLen){
-      console.error("Array audio input stream frame pos: "+this.framePos+" greater then frame length: "+this.arrayAudioBuffer.frameLen);
+    if(this._framePos>this.arrayAudioBuffer.frameLen){
+      console.error("Array audio input stream frame pos: "+this._framePos+" greater then frame length: "+this.arrayAudioBuffer.frameLen);
     }
     return read;
+  }
+
+  get framePos(): number {
+    return this._framePos;
   }
 
 }
