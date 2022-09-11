682151a2d06690e283ba7c96e361cc3e2654d042
klausj <klausj@phonetik.uni-muenchen.de>
2022-09-11 21:52:09 +0200

    Fixed code flow of test.

--- projects/speechrecorderng/src/lib/audio/inddb_audio_buffer.spec.ts
+++ projects/speechrecorderng/src/lib/audio/inddb_audio_buffer.spec.ts
@@ -1,6 +1,7 @@
 // Straight Jasmine testing without Angular's testing support
 import {ArrayAudioBuffer} from "./array_audio_buffer";
 import {IndexedDbAudioBuffer} from "./inddb_audio_buffer";
+import {UUID} from "../utils/utils";
 
 // describe("A suite is just a function", function() {
 //   var a;
@@ -14,41 +15,63 @@ import {IndexedDbAudioBuffer} from "./inddb_audio_buffer";
 
 
 describe('IndexedDbAudioBuffer', () => {
-  let aab: IndexedDbAudioBuffer
+  jasmine.DEFAULT_TIMEOUT_INTERVAL=60000;
+      let CHUNK_COUNT = 1;
+      let CHUNK_SIZE = 2;
+      let NUMBER_OF_CHANNELS = 2;
+      let SAMPLE_RATE=44100;
+      let testRefData = new Array<Float32Array>(NUMBER_OF_CHANNELS);
+      let testData = new Array<Array<Float32Array>>(NUMBER_OF_CHANNELS);
 
-  let CHUNK_COUNT=10000;
-  let CHUNK_SIZE=8192;
-  let NUMBER_OF_CHANNELS=2;
-  let testRefData=new Array<Float32Array>(NUMBER_OF_CHANNELS);
-
-
-  let testData=new Array<Array<Float32Array>>(NUMBER_OF_CHANNELS);
-
-
-for(let ch=0;ch<NUMBER_OF_CHANNELS;ch++) {
-  testRefData[ch] = new Float32Array(CHUNK_COUNT * CHUNK_SIZE);
-  testData[ch]=new Array<Float32Array>();
-  let testRefDataPos=0;
-  for (let ci = 0; ci < CHUNK_COUNT; ci++) {
-    let cc = new Float32Array(CHUNK_SIZE);
-    for (let si = 0; si < CHUNK_SIZE; si++) {
-      cc[si] = Math.random() * 2 - 1;
-    }
-    testData[ch].push(cc);
-    testRefData[ch].set(cc, testRefDataPos);
-    testRefDataPos += CHUNK_SIZE;
-  }
-  //aab = new IndexedDbAudioBufferAudioBuffer();
-}
-  beforeEach(() => {
+      for (let ch = 0; ch < NUMBER_OF_CHANNELS; ch++) {
+        testRefData[ch] = new Float32Array(CHUNK_COUNT * CHUNK_SIZE);
+        testData[ch] = new Array<Float32Array>();
+        let testRefDataPos = 0;
+        for (let ci = 0; ci < CHUNK_COUNT; ci++) {
+          let cc = new Float32Array(CHUNK_SIZE);
+          for (let si = 0; si < CHUNK_SIZE; si++) {
+            cc[si] = Math.random() * 2 - 1;
+          }
+          testData[ch].push(cc);
+          testRefData[ch].set(cc, testRefDataPos);
+          testRefDataPos += CHUNK_SIZE;
+        }
+      }
 
 
-  });
+      beforeEach(() => {
 
-  it('#frames should return correct values', () => {
-      let framesCorrect = true;
-      for (let tt = 0; tt < 2; tt++) {
-        for (let ti = 0; ti < 10; ti++) {
+      });
+
+      it('#frames should return correct values', (done) => {
+        let DB_NAME = 'inddb_audio_buffer_spec_test_db';
+        let RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME = 'rec_file_chunks';
+
+        let aab: IndexedDbAudioBuffer;
+        let or: IDBOpenDBRequest;
+        or = indexedDB.open(DB_NAME, 1);
+        or.onerror = (err) => {
+          console.error("Could not open indexed database: " + DB_NAME + ": " + err);
+        };
+        or.onupgradeneeded = (ev) => {
+          let db = or.result
+          let tr = or.transaction
+
+          if (!db.objectStoreNames.contains(RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME)) {
+            let rfStore = db.createObjectStore(RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME);
+          }
+          if (ev.oldVersion) {
+            console.info("Upgraded indexed database " + DB_NAME + " schema from version " + ev.oldVersion + " to " + ev.newVersion)
+          } else {
+            console.info("Created indexed database " + DB_NAME + " schema version " + ev.newVersion)
+          }
+        };
+        or.onsuccess = () => {
+          console.info("Opened indexed database");
+          //for (let tt = 0; tt < 2; tt++) {
+          //for (let ti = 0; ti < 10; ti++) {
+          let tt = 0;
+          let ti = 0;
           let testPos: number;
           let testBufLength: number;
           if (tt == 0) {
@@ -68,27 +91,67 @@ for(let ch=0;ch<NUMBER_OF_CHANNELS;ch++) {
           for (let ch = 0; ch < NUMBER_OF_CHANNELS; ch++) {
             testBuf[ch] = new Float32Array(testBufLength);
           }
+          let db = or.result;
+          let tr = db.transaction(RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME, 'readwrite');
+          let recFileObjStore = tr.objectStore(RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME);
+          let uuid = UUID.generate();
+          let indDbChkIdx = 0;
+          try {
+            let ch0Data = testData[0];
+            let dataChkCnt = ch0Data.length;
+            let pos = 0;
+            for (let chCkIdx = 0; chCkIdx < dataChkCnt; chCkIdx++) {
+              let bufLen = 0;
+              for (let ch = 0; ch < NUMBER_OF_CHANNELS; ch++) {
+                let chChk = testData[ch][chCkIdx];
+                bufLen = chChk.length;
+                //let cacheId = uuid + '_' + ch + '_' + chCkIdx;
+                let chkDbId = [uuid, indDbChkIdx + chCkIdx, ch];
+                let cr = recFileObjStore.add(chChk, chkDbId);
+                //console.debug("Added: " + ch + " " + (indDbChkIdx + chCkIdx));
+                cr.onsuccess = () => {
+                  //console.debug("Stored audio data to indexed db");
+                }
+                cr.onerror = () => {
+                  console.error("Error storing audio data to indexed db");
+                }
+              }
+              pos += bufLen;
+            }
+            indDbChkIdx += dataChkCnt;
 
-          aab.framesObs(testPos, testBufLength, testBuf).subscribe(
-            {
-              next: (read) => {
-                for (let ch = 0; ch < NUMBER_OF_CHANNELS; ch++) {
-                  for (let i = 0; i < testBufLength; i++) {
-                    if (testBuf[ch][i] !== testRefData[ch][testPos + i]) {
-                      console.error("Frames at " + testPos + i + " differ: " + testBuf[ch][i] + "!==+" + testRefData[ch][testPos + i]);
-                      framesCorrect = false;
-                      break;
+            tr.onerror = (err) => {
+              console.error('Failed to cache audio data to indexed db: ' + err)
+            }
+            tr.oncomplete = () => {
+              console.debug('Transferred capture audio data to indexed db, deleting original data from memory...');
+              aab = new IndexedDbAudioBuffer(db,RECORDING_FILE_CHUNKS_OBJECT_STORE_NAME,NUMBER_OF_CHANNELS,SAMPLE_RATE,CHUNK_SIZE,CHUNK_COUNT+CHUNK_SIZE,uuid);
+              let framesCorrect = true;
+
+              aab.framesObs(testPos, testBufLength, testBuf).subscribe(
+                  {
+                    next: (read) => {
+                      for (let ch = 0; ch < NUMBER_OF_CHANNELS; ch++) {
+                        for (let i = 0; i < testBufLength; i++) {
+                          if (testBuf[ch][i] !== testRefData[ch][testPos + i]) {
+                            console.error("Frames at " + testPos + i + " differ: " + testBuf[ch][i] + "!==+" + testRefData[ch][testPos + i]);
+                            framesCorrect = false;
+                            break;
+                          }
+                        }
+                      }
+                      expect(framesCorrect).toBe(true);
+                      console.debug('Calling done function');
+                      done.call(self);
                     }
                   }
-                }
-                expect(framesCorrect).toBe(true);
-              }
+              );
             }
-          );
+            tr.commit();
+          } catch (err) {
+            console.error('Transfer capture audio data error: ' + err);
+          }
         }
-      }
-
+      });
     }
-      );
-
-});
+);
