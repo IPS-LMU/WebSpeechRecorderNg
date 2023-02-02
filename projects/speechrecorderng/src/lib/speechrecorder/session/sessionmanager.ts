f934f3f1299c618a185a82c94366ec00acc4aa2f
klausj <klausj@phonetik.uni-muenchen.de>
2023-02-02 13:34:35 +0100

    Fixed merge manually.

--- projects/speechrecorderng/src/lib/speechrecorder/session/sessionmanager.ts
+++ projects/speechrecorderng/src/lib/speechrecorder/session/sessionmanager.ts
@@ -314,7 +314,7 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
       this.transportActions.fwdAction.onAction = () => this.nextItem();
       this.transportActions.fwdNextAction.onAction = () => this.nextUnrecordedItem();
       this.transportActions.bwdAction.onAction = () => this.prevItem();
-      this.playStartAction.onAction = () => this.controlAudioPlayer.start();
+      this.playStartAction.onAction = () => this.controlAudioPlayer?.start();
 
     }
 
@@ -384,14 +384,14 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
     return ((this._session!=null) && (this._session.type==='TEST_DEF_A') && (this.audioDevices!=null) && this.audioDevices.length>0)
   }
 
-  set controlAudioPlayer(controlAudioPlayer: AudioPlayer) {
+  set controlAudioPlayer(controlAudioPlayer: AudioPlayer|null) {
     this._controlAudioPlayer=controlAudioPlayer;
     if (this._controlAudioPlayer) {
       this._controlAudioPlayer.listener = this;
     }
   }
 
-  get controlAudioPlayer(): AudioPlayer {
+  get controlAudioPlayer(): AudioPlayer|null {
     return this._controlAudioPlayer;
   }
 
@@ -622,7 +622,9 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
       let adh: AudioDataHolder| null = this._displayRecFile.audioDataHolder;
       if(adh) {
         this.displayAudioClip = new AudioClip(adh);
-        this.controlAudioPlayer.audioClip = this.displayAudioClip;
+        if(this._controlAudioPlayer) {
+          this._controlAudioPlayer.audioClip = this.displayAudioClip;
+        }
         this.showRecording();
       }else {
         // clear for now ...
@@ -683,7 +685,9 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
                       // this.displayAudioClip could have been changed meanwhile, but the recorder unsubcribes before changing the item, therefore there should be no risk to set to wrong item
                       this.displayAudioClip = new AudioClip(fabDh);
                     }
-                    this.controlAudioPlayer.audioClip = this.displayAudioClip
+                    if(this._controlAudioPlayer) {
+                      this._controlAudioPlayer.audioClip = this.displayAudioClip
+                    }
                     this.showRecording();
                   },
                   error: err => {
@@ -723,7 +727,9 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
                     //console.debug("set displayRecFile(): fetch net ab complete, set displayAudioClip.")
                     this.displayAudioClip = new AudioClip(fabDh);
                   }
-                  this.controlAudioPlayer.audioClip = this.displayAudioClip
+                  if(this._controlAudioPlayer) {
+                    this._controlAudioPlayer.audioClip = this.displayAudioClip;
+                  }
                   this.showRecording();
                 },
                 error: err => {
@@ -760,7 +766,9 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
                     // this.displayAudioClip could have been changed meanwhile, but the recorder unsubcribes before changing the item, therefore there should be no risk to set to wrong item
                     this.displayAudioClip = new AudioClip(fabDh);
                   }
-                  this.controlAudioPlayer.audioClip = this.displayAudioClip
+                  if(this._controlAudioPlayer) {
+                    this._controlAudioPlayer.audioClip = this.displayAudioClip
+                  }
                   this.showRecording();
                 },
                 error: err => {
@@ -797,7 +805,9 @@ export class SessionManager extends BasicRecorder implements AfterViewInit,OnDes
                     // this.displayAudioClip could have been changed meanwhile, but the recorder unsubcribes before changing the item, therefore there should be no risk to set to wrong item
                     this.displayAudioClip = new AudioClip(fabDh);
                   }
-                  this.controlAudioPlayer.audioClip = this.displayAudioClip
+                  if(this._controlAudioPlayer) {
+                    this._controlAudioPlayer.audioClip = this.displayAudioClip
+                  }
                   this.showRecording();
                 }, error: err => {
                   console.error("Could not load recording file from server: " + err);
